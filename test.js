38eb2373a5c685d15ee9d88cd4bfdcd173b4a172
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-06 02:42:26 +0900

    write test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+
+var magicn = require('./');
+var test = require('tape');
+
+test('return true', function(t) {
+  var css = '.site-nav > li:hover .dropdown{position:absolute;top:37px;left:0;}';
+
+  t.equal(true, magicn(css));
+
+  t.end();
+});
