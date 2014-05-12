3c81cec86a0c4d4844f20cfc9d2f9fd2a7faaf4c
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-12 10:52:02 +0900

    colored-tape in tape out

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 
 var magicn = require('../');
-var test = require('tape');
+var test = require('colored-tape');
 
 test('return true', function(t) {
   var css = '.site-nav > li:hover .dropdown{position:absolute;top:37px;left:0;}';
