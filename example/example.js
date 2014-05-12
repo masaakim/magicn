8b111f3b5282ce54ce1f00490feac82929795ebf
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-12 11:27:01 +0900

    modify example

--- example/example.js
+++ example/example.js
@@ -3,4 +3,11 @@ var magicn = require('../');
 
 var css = ".site-nav > li:hover .dropdown{position:absolute;top:37px;left:0;}";
 
-magicn(css); // true
+magicn(css);
+/*
+ * [
+ *  { start: { line: 1, column: 1 },
+ *    end: { line: 3, column: 2 },
+ *    source: undefined }
+ *]
+ */
