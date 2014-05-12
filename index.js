df37b187533005f21f70c47c6064f80c2134508d
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-12 10:48:59 +0900

    change response to magic number positions

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 module.exports = function(css) {
   var parse = require('css-parse');
+  var rework = require('rework')
   var unit = require('css-unit');
-  var ast = parse(css);
+
+  var ast = parse(css, {position: true});
   ast = ast.stylesheet;
 
-  var ret = false;
+  var pos = [];
 
   matchedProp = [
     'top',
@@ -35,12 +37,13 @@ module.exports = function(css) {
       matchedProp.forEach(function(prop) {
         if (declaration.property === prop) {
           var num = unit.rm(declaration.value);
-          if (num % 10) ret = true;
+
+          if (num % 10) pos.push(rule.position);
         }
       });
     });
   });
 
-  return ret;
+  return pos;
 
 };
