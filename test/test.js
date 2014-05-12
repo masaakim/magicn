44ca62f31966aa03901c9ae2a697c0f8669c1390
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-12 10:54:52 +0900

    fix test code

--- test/test.js
+++ test/test.js
@@ -5,7 +5,14 @@ var test = require('colored-tape');
 test('return true', function(t) {
   var css = '.site-nav > li:hover .dropdown{position:absolute;top:37px;left:0;}';
 
-  t.equal(true, magicn(css));
+  var result = magicn(css);
+  var expected = [
+    { start: { line: 1, column: 1 },
+      end: { line: 1, column: 67 },
+      source: undefined }
+  ];
+
+  t.same(result, expected);
 
   t.end();
 });
